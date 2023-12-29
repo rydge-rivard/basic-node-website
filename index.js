babaf0b82a209d85edc38fcf4773a7590891cd49
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2023-12-29 11:51:12 -0500

    Add file routing via server url.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let http = require("http");
 let fs = require("fs");
+let url = require("url");
 
 http
   .createServer(function (req, res) {
-    const fileName = "Pages/index.html";
-    fs.readFile(fileName, function (err, data) {
+    let q = url.parse(req.url, true);
+    let filename = "." + q.pathname + ".html";
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404.html");
+      }
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       return res.end();
